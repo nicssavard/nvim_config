97e4d75dabebe12571c635d7c0b7984a816bbeee
Nicolas Savard <nsavard@k2geospatial.com>
2025-10-11 05:27:12 -0400

    remove hover error message

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -73,7 +73,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 -- Diagnostics
 vim.diagnostic.config({
-  virtual_lines = {
-    current_line = true,
-  },
+  -- virtual_lines = {
+  --   current_line = false,
+  -- },
 })
